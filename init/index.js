5d728b737b0b9a20259bda83d07c5aa0f2ba6732
Laxman6375 <laxmansuthar365@gmail.com>
2023-11-17 14:22:03 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const initData = require("./data");
+const Listing = require("../models/listing");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/waderlust";
+main()
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "65462e68ab66c36021b8b305",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+
+initDB();
